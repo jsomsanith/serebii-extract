b6b8b9ed58764bf363ce14e0836df01fe7b9e513
Jimmy Somsanith <jimmy.somsanith@komodohealth.com>
2022-10-02 17:42:33 +0200

    fix: extract dex command and add logs

--- src/download-event-dex.ts
+++ src/download-event-dex.ts
@@ -14,9 +14,13 @@ const MAX_POKEDEX_ID = 898;
 
 	for (let i = 1; i <= MAX_POKEDEX_ID; i++) {
 		const pokedexId = String(i).padStart(3, '0');
-		const res = await fetch(SEREBII_EVENT_URL_TEMPLATE.replace(PLACEHOLDER, '150'));
+		const serebiiUrl = SEREBII_EVENT_URL_TEMPLATE.replace(PLACEHOLDER, '150');
+		console.log(`${pokedexId}: Fetching ${serebiiUrl}`);
+		const res = await fetch(serebiiUrl);
 		const serebiiDom = await res.text();
 
-		fs.writeFileSync(path.join(TARGET_HTML_PATH, `${pokedexId}.shtml`), serebiiDom);
+		const eventDexFilePath = path.join(TARGET_HTML_PATH, `${pokedexId}.shtml`);
+		fs.writeFileSync(eventDexFilePath, serebiiDom);
+		console.log(`${pokedexId}: saved in ${eventDexFilePath}\n`);
 	}
 })();
